79f394119783f8c675abd709f74ae30bd340a6a3
Alexander Dickhans <adickhans@gmail.com>
2025-02-22 15:06:46 -0700

    Initial Commit

---
+++ EchoPulse/EchoPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoPulseApp.swift
+//  EchoPulse
+//
+//  Created by Alexander Dickhans on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct EchoPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
