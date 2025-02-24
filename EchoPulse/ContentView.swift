81bf969f13e724efe76ac4db170809c7797d0968
Alexander Dickhans <adickhans@gmail.com>
2025-02-23 23:44:56 -0700

    Working on match fetching

--- EchoPulse/ContentView.swift
+++ EchoPulse/ContentView.swift
@@ -12,7 +12,7 @@ import SwiftUI
 class TeamEvents: ObservableObject {
     @Published var event_indexes: [String]
     @Published var events: [Event]
-    
+
     init(team: Team? = nil) {
         event_indexes = [String]()
         events = [Event]()
@@ -35,16 +35,20 @@ struct ContentView: View {
     @State var matchList: [CompetitionAttributes.Match]
     @State var team: String
     @State var competitions: TeamEvents? = nil
+    @State var event: Event? = nil
+    @State var division: Division? = nil
 
     var body: some View {
         NavigationView {
             VStack {
-                ZStack (alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+                ZStack(
+                    alignment: Alignment(horizontal: .center, vertical: .bottom)
+                ) {
                     Color.orange
-                                    .frame(height: 100) // Height of the top bar
-                                Text("Echo Live")
-                                    .foregroundColor(.white) // Text color
-                                    .padding().font(.system(size: 18, weight: .heavy))
+                        .frame(height: 100)  // Height of the top bar
+                    Text("Echo Live")
+                        .foregroundColor(.white)  // Text color
+                        .padding().font(.system(size: 18, weight: .heavy))
                 }.edgesIgnoringSafeArea(.top)
                 ScrollView {
                     VStack {
@@ -54,14 +58,20 @@ struct ContentView: View {
                                     await stopActivity()
                                 }
                             }
+                            Text(
+                                activity?.attributes.name
+                                    ?? "No activity selected")
                             FullDetailedView(
                                 matches: matchList, teamName: team)
                         } else {
-                            TextField("2654E", text: $team).frame(alignment: .center).multilineTextAlignment(.center).font(.system(size: 36))
+                            TextField("2654E", text: $team).frame(
+                                alignment: .center
+                            ).multilineTextAlignment(.center).font(
+                                .system(size: 36))
                             Button("Search") {
                                 findCompetitions()
                             }
-                            buildCompetitionsList()
+                            buildCompetitionsList().buttonStyle(.borderless)
                         }
                     }.buttonStyle(.borderedProminent).controlSize(.large)
                 }
@@ -69,41 +79,76 @@ struct ContentView: View {
         }
 
     }
-    
+
     func buildCompetitionsList() -> some View {
         VStack(spacing: 0) {
             Group {
                 if let events = competitions?.events {
                     ForEach(events, id: \.id) { competition in
-                        Button(action: startActivity) {
-                            Text(competition.name)
+                        Button(action: { () -> Void in
+                            self.startActivity(event: competition)
+                        }) {
+                            Text(competition.name).multilineTextAlignment(
+                                .leading
+                            ).padding(10)
                         }
                     }
                 }
             }
         }
     }
-    
+
     func findCompetitions() {
-        let fetched_team = Team(number: self.team)
-        let fetched_events = TeamEvents(team: fetched_team)
-        
-        Task {
-            self.competitions = fetched_events
+        let fetched_team = Team(number: self.team.uppercased())
+
+        if fetched_team.registered {
+            let fetched_events = TeamEvents(team: fetched_team)
+
+            Task {
+                self.competitions = fetched_events
+            }
         }
     }
 
-    func startActivity() {
-        let attributes = CompetitionAttributes.preview
+    func startActivity(event: Event) {
+        let attributes = CompetitionAttributes(
+            name: event.name, division: event.divisions[0].name)
         let state = ActivityContent(
             state: CompetitionAttributes.ContentState.pout,
             staleDate: Date().addingTimeInterval(60))
+        
+        self.event = event
 
         do {
             activity = try Activity<CompetitionAttributes>.request(
                 attributes: attributes, content: state, pushType: nil)
         } catch {
             print(error.localizedDescription)
+        }
+    }
+
+    func updateMatchList() async {
+        self.event!.fetch_matches(division: self.division!)
+        
+        let matches = self.event!.matches
+        
+        var fetched_matches: [CompetitionAttributes.Match] = []
+        
+        matches.forEach { match in
+            let new_match = CompetitionAttributes.Match()
+            fetched_matches.append(new_match)
+        }
+        
+        self.matchList = fetched_matches
+    }
+
+    private func runTask() {
+        guard activity != nil else { return }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 30.0) {
+            Task { await updateMatchList() }
+
+            self.runTask()
         }
     }
 
@@ -125,3 +170,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
